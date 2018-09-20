d54303b584e1f585740b60d2b005dfcd46d00488
d.kanai <d.kanai@pastoraldog.com>
2018-09-20 13:10:09 +0900

    refactor to junit5

--- java_junit/src/test/java/UserSignatureTest.java
+++ java_junit/src/test/java/UserSignatureTest.java
@@ -1,26 +1,27 @@
-import org.junit.experimental.runners.Enclosed;
-import org.junit.experimental.theories.DataPoints;
-import org.junit.experimental.theories.Theories;
-import org.junit.experimental.theories.Theory;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(Enclosed.class)
 public class UserSignatureTest {
 
-    @RunWith(Theories.class)
+    @Nested
     public static class ユーザの妥当性検証 {
-        @DataPoints
-        public static Fixture[] patterns = {
-                new Fixture("password", true),
-                new Fixture("passwor", false),
-        };
+        static List<Fixture> patterns() {
+            return Arrays.asList(
+                    new Fixture("password", true),
+                    new Fixture("passwor", false)
+            );
+        }
 
-        @Theory
-        public void パスワード閾値テスト(Fixture pattern) {
+        @ParameterizedTest
+        @MethodSource("patterns")
+        void パスワード閾値テスト(Fixture pattern) {
             //given
             User user = new UserBuilder().setPassword(pattern.password).build();
             //when
@@ -32,7 +33,6 @@ public class UserSignatureTest {
         private static class Fixture {
             private String password;
             private boolean expected;
-
             public Fixture(String password, boolean expected) {
                 this.password = password;
                 this.expected = expected;
@@ -40,26 +40,22 @@ public class UserSignatureTest {
         }
 
         private class UserBuilder {
-
             private String password;
-
             public UserBuilder setPassword(String password) {
                 this.password = password;
                 return this;
             }
-
             public User build() {
                 return new User("", "", password);
             }
         }
     }
 
-    @RunWith(Theories.class)
+    @Nested
     public static class 署名の取得 {
 
-        @DataPoints
-        public static UserDataBuilder[] patterns(){
-            return new UserDataBuilder[] {
+        static UserDataBuilder[] patterns() {
+            return new UserDataBuilder[]{
                     new UserDataBuilder().setExpected("tanaka: PD OK"),
                     new UserDataBuilder().setName("yamada").setExpected("yamada: PD OK"),
                     new UserDataBuilder().setCompanyName("MJ").setExpected("tanaka: MJ OK"),
@@ -68,8 +64,9 @@ public class UserSignatureTest {
             };
         }
 
-        @Theory
-        public void 署名取得パターン(UserDataBuilder pattern){
+        @ParameterizedTest
+        @MethodSource("patterns")
+        public void 署名取得パターン(UserDataBuilder pattern) {
             //given
             User user = new User(pattern.name, pattern.companyName, pattern.password);
             //when
@@ -103,11 +100,7 @@ public class UserSignatureTest {
                 this.expected = s;
                 return this;
             }
-
-
         }
-
-
     }
 }
 
