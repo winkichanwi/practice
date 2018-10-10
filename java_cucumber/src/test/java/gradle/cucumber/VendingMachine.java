d8deaf09509034341a2cadedb8aa4148b5e780a1
d.kanai <d.kanai@pastoraldog.com>
2018-10-10 09:39:42 +0900

    clean for work

--- java_cucumber/src/test/java/gradle/cucumber/VendingMachine.java
+++ java_cucumber/src/test/java/gradle/cucumber/VendingMachine.java
@@ -4,55 +4,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class VendingMachine {
-    private final Menu menu = new Menu();
-    private int charge;
-    public List<Drink> drinks = new ArrayList<Drink>();
 
-    public VendingMachine charge(Integer money) {
-        if (canUseMoney(money)) {
-            this.charge += money;
-        }
-        return this;
-    }
-
-    private boolean canUseMoney(Integer money) {
-        return money == 10 || money == 50 || money == 100 || money == 500 || money == 1000;
-    }
-
-    public Integer currentCharge() {
-        return charge;
-    }
-
-    public int reset() {
-        int change = charge;
-        charge = 0;
-        return change;
-    }
-
-    public void addDrink(String name, Integer number) {
-        this.drinks.add(new Drink(name));
-    }
-
-    public void buy(String name) {
-        charge = charge - menu.priceBy(name);
-        drinks.remove(new Drink(name));
-    }
-
-    public int inventorySizeBy(String name) {
-        return (int) drinks.stream().filter(drink->drink.name.equals(name)).count();
-    }
-
-    private class Drink {
-        private final String name;
-
-        public Drink(String name) {
-            this.name = name;
-        }
-
-        public boolean equals(Object obj) {
-            Drink drink = (Drink) obj;
-            return drink.name.equals(name);
-        }
-
-    }
 }
--- java_cucumber/src/test/resources/vendingmachine.feature
+++ java_cucumber/src/test/resources/vendingmachine.feature
@@ -1,46 +1,6 @@
 Feature: 自動販売機
 
-  Background:
-    Given 自動販売機がある
-    And "コーラ"を1個追加する
-    And "お茶"を1個追加する
-
   Scenario: 入金額確認
+    Given 自動販売機がある
     When 100円を入金
     Then 100円が入金されている
-
-  Scenario: 複数入金額確認
-    When 100円を入金
-    And  500円を入金
-    Then 600円が入金されている
-
-  Scenario: 不正なお金を入金
-    When 以下のお金を入金
-        |1|
-        |5|
-        |5000|
-        |10000|
-    Then 0円が入金されている
-
-  Scenario: お釣りの返却ができる
-    Given 100円を入金
-    And 500円を入金
-    When お釣りを返却
-    Then 600円が返却される
-    And 0円が入金されている
-
-  Scenario: 在庫が確認できる
-    Then "コーラ"の在庫が1個
-    And "お茶"の在庫が1個
-
-  Scenario Outline: 購入できる
-    Given 500円を入金
-    When "<名前>"を購入
-    Then "<名前>"の在庫が0個
-    And <お釣り>円が入金されている
-
-    Examples:
-        |名前|お釣り|
-        |コーラ|380|
-        |お茶  |350|
-
